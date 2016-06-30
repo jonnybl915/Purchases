9172abdd98f30facad4a2e00c9dbedb16b18f4f4
jonathandavidblack <jonnybl915@gmail.com>
2016-06-30 10:32:41 -0400

    add paging with bug

--- src/main/java/com/theironyard/PurchaseRepository.java
+++ src/main/java/com/theironyard/PurchaseRepository.java
@@ -1,10 +1,15 @@
 package com.theironyard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Created by jonathandavidblack on 6/22/16.
  */
-public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
-    public Iterable<Purchase> findByCategory(String Category);
+public interface PurchaseRepository extends PagingAndSortingRepository<Purchase, Integer> { //CrudRepository<Purchase, Integer> --removed this
+    Page<Purchase> findByCategory(Pageable pageable, String Category); //changed from Iterable to Page, and inserted Pageable pageable
+
+
 }
--- src/main/java/com/theironyard/PurchasesController.java
+++ src/main/java/com/theironyard/PurchasesController.java
@@ -1,6 +1,8 @@
 package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,19 +49,31 @@ public class PurchasesController {
         }
     }
 
-        @RequestMapping(path = "/", method = RequestMethod.GET)
-        public String home (Model model, String category) {
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home (Model model, String category, Integer page) {
 
-            Iterable<Purchase> purchase;
-            if (category != null) {
-                purchase = purchases.findByCategory(category);
-            }
-            else {
-               purchase = purchases.findAll();
-            }
-            model.addAttribute("purchases", purchase);
+        page = (page == null) ? 0 : page; //ternary operator
 
-            return "home";
+        PageRequest pr = new PageRequest(page, 10);
+
+        Page<Purchase> purchase;
+
+        if (category != null) {
+            purchase = purchases.findByCategory(pr, category);
         }
+        else {
+           purchase = purchases.findAll(pr);
+        }
+
+        model.addAttribute("purchases", purchase);
+        model.addAttribute("category", category);
+
+        model.addAttribute("nextPage", page + 1);
+        model.addAttribute("showNext". purchase.hasNext());
+
+        model.addAttribute("prevPage", page - 1);
+        model.addAttribute("showPrev". purchase.hasPrevious());
+        return "home";
+    }
 }
 
